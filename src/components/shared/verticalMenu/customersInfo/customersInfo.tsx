dd12c98d88cf218d388d37a77c3edd5ddb01e6d4
Katsiaryna_Kalinina <Katsiaryna_Kalinina@epam.com>
2020-03-03 17:07:38 +0300

    Fix html table issue in console.

--- src/components/shared/verticalMenu/customersInfo/customersInfo.tsx
+++ src/components/shared/verticalMenu/customersInfo/customersInfo.tsx
@@ -35,22 +35,24 @@ export const CustomersInfo = () => {
       <div className="work-time">
         <h5>Режим работы</h5>
         <table>
-          <tr>
-            <th>День</th>
-            <th>Время работы</th>
-          </tr>
-          <tr>
-            <td>Пн-Пт</td>
-            <td>09:00 — 20:00</td>
-          </tr>
-          <tr>
-            <td>Сб</td>
-            <td>11:00 — 18:00</td>
-          </tr>
-          <tr>
-            <td>Вс</td>
-            <td>Выходной</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>День</th>
+              <th>Время работы</th>
+            </tr>
+            <tr>
+              <td>Пн-Пт</td>
+              <td>09:00 — 20:00</td>
+            </tr>
+            <tr>
+              <td>Сб</td>
+              <td>11:00 — 18:00</td>
+            </tr>
+            <tr>
+              <td>Вс</td>
+              <td>Выходной</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
