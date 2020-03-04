12bf1ddb1cdbb627f7d46c0cda9845bc0a008ace
Katsiaryna_Kalinina <Katsiaryna_Kalinina@epam.com>
2020-03-04 10:20:03 +0300

    Fix menu items styles.

--- src/components/shared/horizontalMenu/horizontalMenu.tsx
+++ src/components/shared/horizontalMenu/horizontalMenu.tsx
@@ -8,20 +8,39 @@ import { NavLink } from 'react-router-dom';
 export const HorizontalMenu = () => {
   return (
     <Navbar collapseOnSelect expand="lg" id="horizontal-menu">
-      <NavLink to="/">Главная</NavLink>
+      <div>
+        <NavLink className="first-hor-menu-item" to="/">
+          Главная
+        </NavLink>
+      </div>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" id="horizontal-menu-items">
-          <NavDropdown title="Товары и услуги" id="horizontal-menu-dropdown">
-            <NavLink to="/">1</NavLink>
-            <NavLink to="/">2</NavLink>
-            <NavLink to="/">3</NavLink>
-            <NavDropdown.Divider />
-            <NavLink to="/">4</NavLink>
-          </NavDropdown>
-          <NavLink to="/about-us">О нас</NavLink>
-          <NavLink to="/contacts">Контакты</NavLink>
-          <NavLink to="/delivery">Доставка и оплата</NavLink>
+          <div className="hor-menu-item">
+            <NavDropdown title="Товары и услуги" id="horizontal-menu-dropdown">
+              <div className="hor-menu-subitem">
+                <NavLink to="/">1</NavLink>
+              </div>
+              <div className="hor-menu-subitem">
+                <NavLink to="/">2</NavLink>
+              </div>
+              <div className="hor-menu-subitem">
+                <NavLink to="/">3</NavLink>
+              </div>
+              <div className="hor-menu-subitem">
+                <NavLink to="/">4</NavLink>
+              </div>
+            </NavDropdown>
+          </div>
+          <div className="hor-menu-item">
+            <NavLink to="/about-us">О нас</NavLink>
+          </div>
+          <div className="hor-menu-item">
+            <NavLink to="/contacts">Контакты</NavLink>
+          </div>
+          <div className="hor-menu-item">
+            <NavLink to="/delivery">Доставка и оплата</NavLink>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/components/shared/verticalMenu/menu/menu.tsx
+++ src/components/shared/verticalMenu/menu/menu.tsx
@@ -6,12 +6,24 @@ import { NavLink } from 'react-router-dom';
 export const Menu = () => {
   return (
     <Nav defaultActiveKey="/home" className="flex-column" id="vertical-menu">
-      <NavLink to="/">Меню</NavLink>
-      <NavLink to="/">Товары и услуги</NavLink>
-      <NavLink to="/about-us">О нас</NavLink>
-      <NavLink to="/">Сертификаты и лицензии</NavLink>
-      <NavLink to="/feedbacks">Отзывы</NavLink>
-      <NavLink to="/delivery">Доставка и оплата</NavLink>
+      <div className="vert-menu-item">
+        <NavLink to="/">Меню</NavLink>
+      </div>
+      <div className="vert-menu-item">
+        <NavLink to="/">Товары и услуги</NavLink>
+      </div>
+      <div className="vert-menu-item">
+        <NavLink to="/about-us">О нас</NavLink>
+      </div>
+      <div className="vert-menu-item">
+        <NavLink to="/">Сертификаты и лицензии</NavLink>
+      </div>
+      <div className="vert-menu-item">
+        <NavLink to="/feedbacks">Отзывы</NavLink>
+      </div>
+      <div className="vert-menu-item">
+        <NavLink to="/delivery">Доставка и оплата</NavLink>
+      </div>
     </Nav>
   );
 };
